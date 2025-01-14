7a8da79b536f7c09857cfdb6fc58e03e540d6751
Martín Pravia <marto.pravia@gmail.com>
2025-01-14 19:49:14 +0000

    se deja cargada la funcion del fetch a planets y la maquetación, pero por algun motivo el fetch no funciona

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,61 +1,82 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			urlBaseStarWars: "https://www.swapi.tech/api",
-			people: JSON.parse(localStorage.getItem("people")) || [],
-			favorites: []
-		},
-		actions: {
-			getAllPeople: async () => {
-				const store = getStore()
-				if(store.people <=0) {
-					try {
-						const response = await fetch(`${store.urlBaseStarWars}/people`)
-						const data = await response.json()
-						console.log(data)
-						console.log(data.results)
-						for(let person of data.results) {
-							console.log(person)
-							const responsePerson = await fetch(person.url)
-							const dataPerson = await responsePerson.json()
-							console.log(dataPerson)
-							setStore({
-								people: [...store.people, dataPerson.result]
-							})
-							localStorage.setItem("people", JSON.stringify(store.people))
-						}
-					} catch (error) {
-						console.log(error)
-					}
-				}
-				
-			},
-			addFavorites: (favorite) => {
-				const store = getStore()
-				const exist = store.favorites.some((item)=> item._id == favorite._id)
-				if (exist) {
-					console.log("Ya existe el favorito")
-					// alert("Ya existe el favorito")
-					const newFavorite = store.favorites.filter((item) => item._id != favorite._id)
-					setStore({
-						favorites: newFavorite
-					})
-					alert("Favorito Eliminado")
-				}else{
-					console.log("Favorito agregado")
-					alert("Favorito agregado")
-					setStore({
-						favorites: [...store.favorites, favorite]
-					})
-				}
+  return {
+    store: {
+      urlBaseStarWars: "https://www.swapi.tech/api",
+      people: JSON.parse(localStorage.getItem("people")) || [],
+      planets: JSON.parse(localStorage.getItem("planets")) || [],
+      favorites: [],
+    },
+    actions: {
+      getAllPeople: async () => {
+        const store = getStore();
+        if (store.people <= 0) {
+          try {
+            const response = await fetch(`${store.urlBaseStarWars}/people`);
+            const data = await response.json();
+            console.log(data);
+            console.log(data.results);
+            for (let person of data.results) {
+              console.log(person);
+              const responsePerson = await fetch(person.url);
+              const dataPerson = await responsePerson.json();
+              console.log(dataPerson);
+              setStore({
+                people: [...store.people, dataPerson.result],
+              });
+              localStorage.setItem("people", JSON.stringify(store.people));
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      },
 
-				},
+      getAllPlanets: async () => {
+        console.log("Ejecutando getAllPlanets");
+        const store = getStore();
+        try {
+          const response = await fetch(`${store.urlBaseStarWars}/planets`);
+          const data = await response.json();
+          console.log("Data recibida", data);
+          console.log(data.results);
+          for (let planet of data.results) {
+            console.log(planet);
+            const responsePlanet = await fetch(planet.url);
+            const dataPlanet = await responsePlanet.json();
+            console.log(dataPlanet);
+            setStore({
+              planets: [...store.planets, dataPlanet.result],
+            });
+            localStorage.setItem("planets", JSON.stringify(store.planets));
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      },
 
-
-			}
-			
-		}
-	};
-
+      addFavorites: (favorite) => {
+        const store = getStore();
+        const exist = store.favorites.some((item) => item._id == favorite._id);
+        if (exist) {
+          console.log("Ya existe el favorito");
+          // alert("Ya existe el favorito")
+          const newFavorite = store.favorites.filter(
+            (item) => item._id != favorite._id
+          );
+          setStore({
+            favorites: newFavorite,
+          });
+          alert("Favorito Eliminado");
+        } else {
+          console.log("Favorito agregado");
+          alert("Favorito agregado");
+          setStore({
+            favorites: [...store.favorites, favorite],
+          });
+        }
+      },
+    },
+  };
+};
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,28 +6,76 @@ import { Link, useParams } from "react-router-dom";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
+
   return (
     <div className="container">
-      <h1 className="text-danger">Characters</h1>
-      <div className="carousel border border-dark-subtle">
-        {store.people.map((item) => {
-          return (
-            <div className="my-card mx-2" key={item._id}>
-              <img
-                src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`}
-                alt="{item.properties.name}"
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <h5 className="card-title">{item.properties.name}</h5>
-                <p className="card-text">Gender: {item.properties.gender}</p>
-                <p className="card-text">
-                  Hair color: {item.properties.hair_color}
-                </p>
-                <p className="card-text">
-                  Eye color: {item.properties.eye_color}
-                </p>
-                <div className="d-flex justify-content-between">
+      <div>
+        <h1 className="text-danger">Characters</h1>
+        <div className="carousel border border-dark-subtle">
+          {store.people.map((item) => {
+            return (
+              <div className="my-card mx-2" key={item._id}>
+                <img
+                  src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`}
+                  alt="{item.properties.name}"
+                  className="card-img-top"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{item.properties.name}</h5>
+                  <p className="card-text">Gender: {item.properties.gender}</p>
+                  <p className="card-text">
+                    Hair color: {item.properties.hair_color}
+                  </p>
+                  <p className="card-text">
+                    Eye color: {item.properties.eye_color}
+                  </p>
+                  <div className="d-flex justify-content-between">
+                    <Link
+                      to={`/people/${item._id}`}
+                      className="btn btn-outline-primary"
+                    >
+                      Learn more!
+                    </Link>
+                    <button
+                      className="btn btn-outline-secondary"
+                      onClick={() => actions.addFavorites(item)}
+                    >
+                      <i
+                        className={`fa-heart ${
+                          store.favorites.some((fav) => fav._id === item._id)
+                            ? "fa-solid text-danger"
+                            : "fa-regular text-warning"
+                        }`}
+                      ></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-danger mt-3">Planets</h2>
+      
+
+        <div className="carousel border border-dark-subtle">
+          {store.planets.map((pla) => {
+            return (
+              <div className="my-card mx-2" key={pla._id}>
+                <img
+                  src={`https://starwars-visualguide.com/assets/img/planets/${pla.uid}.jpg`}
+                  alt="{pla.properties.name}"
+                  className="card-img-top"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{pla.properties.name}</h5>
+                  <p className="card-text">Population: {pla.properties.population}</p>
+                  <p className="card-text">
+                    Terrain: {pla.properties.terrain}
+                  </p>
+                 
+                  <div className="d-flex justify-content-between">
                   <Link
                     to={`/people/${item._id}`}
                     className="btn btn-outline-primary"
@@ -36,21 +84,22 @@ export const Home = () => {
                   </Link>
                   <button
                     className="btn btn-outline-secondary"
-                    onClick={() => actions.addFavorites(item)}
+                    onClick={() => actions.addFavorites(pla)}
                   >
                     <i
                       className={`fa-heart ${
-                        store.favorites.some((fav) => fav._id === item._id)
+                        store.favorites.some((fav) => fav._id === pla._id)
                           ? "fa-solid text-danger"
                           : "fa-regular text-warning"
                       }`}
                     ></i>
                   </button>
                 </div>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
