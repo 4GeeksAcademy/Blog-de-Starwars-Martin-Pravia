55ee3fca009339711c8f9c3a2b73049db564a117
Martín Pravia <marto.pravia@gmail.com>
2025-01-14 12:42:48 +0000

    se crea diseño del carousel principal de characters

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,37 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			urlBaseStarWars: "https://www.swapi.tech/api",
+			people: JSON.parse(localStorage.getItem("people")) || []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getAllPeople: async () => {
+				const store = getStore()
+				if(store.people <=0) {
+					try {
+						const response = await fetch(`${store.urlBaseStarWars}/people`)
+						const data = await response.json()
+						console.log(data)
+						console.log(data.results)
+						for(let person of data.results) {
+							console.log(person)
+							const responsePerson = await fetch(person.url)
+							const dataPerson = await responsePerson.json()
+							console.log(dataPerson)
+							setStore({
+								people: [...store.people, dataPerson.result]
+							})
+							localStorage.setItem("people", JSON.stringify(store.people))
+						}
+					} catch (error) {
+						console.log(error)
+					}
+				}
+				
+			}
 			}
 		}
 	};
-};
+
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,34 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  return (
+    <div className="container">
+      <h1 className="text-danger">Characters</h1>
+      <div className="carousel border border-dark-subtle">
+        {store.people.map((item)=> {
+            return (
+                <div className="my-card mx-2" key={item._id}>
+                    <img src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`} alt="{item.properties.name}" className="card-img-top" />
+                    <div className="card-body">
+                    <h5 className="card-title">{item.properties.name}</h5>
+                    <p className="card-text">Gender: {item.properties.gender}</p>
+                    <p className="card-text">Hair color: {item.properties.hair_color}</p>
+                    <p className="card-text">Eye color: {item.properties.eye_color}</p>
+                    <div className="d-flex justify-content-between">
+                    <button  className="btn btn-outline-primary">Learn more!</button>
+                    <button  className="btn btn-outline-warning"><i class="bi bi-heart"></i></button>
+                    </div>
+
+                </div>
+                </div>
+
+            )
+        })}
+      </div>
+    </div>
+  );
+};
